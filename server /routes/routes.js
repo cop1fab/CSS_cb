675251d2c3feb9a268cee617de3de8983249b3c1
Cop1fab <bienaimecopain@gmail.com>
2019-01-07 16:23:47 +0200

    [ch-163022890] added routes

--- server /routes/routes.js
+++ server /routes/routes.js
@@ -1,14 +1,35 @@
-import {
-  Router
-} from 'express';
-import users from '../models';
-import meetups from '../models ';
-import rsvp from '../models ';
-import questions from '../models ';
+const express = require('express');
+const controller = require('../controllers/index');
 
-const routers = Router();
+const router = express.Router();
 
-routers.use(users, meetups, rsvp, questions);
+//Admin can create meetups and request to see all meetups. 
+router.route('/api/v1/meetups')
+  .post(controller.meetupCreate)
+  .get(controller.getAllMeetups);
 
+//filtering out upcoming meetups. 
+router.route('/api/v1/meetups/upcoming/')
+  .get(controller.getUpcomingMeetups);
 
-export default routers;
+//getting one meetup per id. 
+router.route('/api/v1/meetups/:id')
+  .get(controller.getMeetupById);
+
+//Asking a question 
+router.route('/api/v1/questions')
+  .post(controller.createQuestion);
+
+//upvote a question 
+router.route('/api/v1/questions/:id/upvote')
+  .patch(controller.upVote);
+
+//downvote a question
+router.route('/api/v1/questions/:id/downvote')
+  .patch(controller.downVote);
+
+//
+router.route('/api/v1/meetups/:id/rsvps')
+  .post(controller.rsvps);
+
+module.exports = router;
